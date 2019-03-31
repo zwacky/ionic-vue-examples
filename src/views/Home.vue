fec61c840fc8284352af858ed398c0eca9cc3fb3
Simon <simon.wicki@justwatch.com>
2019-03-31 12:58:51 +0200

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <IonVuePage :title="'@ionic/vue examples'" :showBackButton="false">
     <ion-list>
-      <ion-item v-for="(link, i) in links" :key="i">
+      <ion-item 
+	  	v-for="(link, i) in links" 
+		:key="i"
+		:class="{ selected: link === selectedLink }"
+		@mouseenter="enter(link)"
+	  >
         <router-link :to="toUrl(link)">{{ link }}</router-link>
       </ion-item>
     </ion-list>
@@ -13,12 +18,22 @@ export default {
   data() {
     return {
       links: ['Action Sheet', 'Alert', 'Loading', 'Menu', 'Modal', 'Popover'],
+      selectedLink: null,
     }
   },
   methods: {
     toUrl(str) {
       return str.toLowerCase().replace(' ', '-')
     },
+    enter(link) {
+      this.selectedLink = link
+    },
   },
 }
 </script>
+
+<style>
+.selected {
+  font-size: 30px;
+}
+</style>
